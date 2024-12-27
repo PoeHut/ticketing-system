70ab78c98d6b24cefe6b81ac7efb8fce12674148
poeeiphyu <poeeiphyu@mmbbank.com.mm>
2024-12-27 16:50:00 +0630

    ADD:Adding meta data for tab

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
 
+export const metadata = {
+  title: "Page Not Found",
+};
+
 export default function NotFound() {
   return (
-    <div>
+    <div className="flex flex-col mx-auto mt-10 justify-center items-center gap-4">
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
-      <Link href="/home">Return Home</Link>
+      <Link href="/home" className="text-blue-700 cursor-pointer font-semibold">
+        Return Home
+      </Link>
     </div>
   );
 }
